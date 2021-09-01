2f9df94e98e0c2524b4a31ced1030f765966365a
Dom <dominicemandy@gmail.com>
2021-08-31 19:43:53 -0700

    update copy

--- code/coins.ts
+++ code/coins.ts
@@ -1,18 +1,18 @@
 export class Coin {
   // generate one bit of cryptographic pseudo-randomness
-  #getRandomBit() {
-    const bits = new Uint32Array(1);
-    const [bit] = crypto.getRandomValues(bits);
-    return !!(bit % 2);
+  #getRandomBoolean() {
+    const integers = new Uint32Array(1);
+    const [random] = crypto.getRandomValues(integers);
+    return !!(random % 2);
   }
-  // flip the coin by unbiasing the entropy
+  // flip the coin fairly
   // https://en.wikipedia.org/wiki/Fair_coin#Fair_results_from_a_biased_coin
   // von Neumann, John(1951)."Various techniques used in connection with random digits". National Bureau of Standards Applied Math Series. 12: 36.
   toss() {
     let bit;
     do {
-      bit = this.#getRandomBit();
-    } while (bit === this.#getRandomBit());
+      bit = this.#getRandomBoolean();
+    } while (bit === this.#getRandomBoolean());
     return bit;
   }
 }
